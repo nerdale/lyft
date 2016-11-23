0282f6e07ba132502e1b3c550213a6fbbd21bbe2
“nerdale” <alexandra.neira@gmail.com>
2016-11-23 13:01:48 -0300

    probando boton next signup

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 	$('select').material_select();
 });
+
+
 //validacion telefono numeros
-$('#formulario').submit(function () {  
+/*$('#formulario').submit(function () {  
     if($('#chile').val().length < 9) {  
         alert("El teléfono debe tener 9 caracteres. Ej. 666112233");  
         return false;  
     }  
     return false;  
-});  
+});  */
